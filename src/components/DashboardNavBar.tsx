e8aa74b70c73e83049c0b2436066a6a89eac7fea
Tuhin Mondal <tuhinchess@gmail.com>
2023-05-17 19:32:14 +0530

    invite button added

--- src/components/DashboardNavBar.tsx
+++ src/components/DashboardNavBar.tsx
@@ -10,21 +10,29 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Image from "react-bootstrap/Image";
 import { Modal } from "react-bootstrap";
 import { useState } from 'react';
+import { cryptoUtils } from "../App";
+import {
+	getUserKeyStoreFromServerAndInitKeyStore,
+	sendGroupKeyToServer,
+	sendPreKeyBundleAndUserKeyStoreToServer,
+} from "../utils/networkUtils";
 
-export default function DashboardNavBar(){
 
-    const [showModal, setShowModal] = useState(false);
-    const handleModalClose = () => {
-        setShowModal(false);
-    };
+export default function DashboardNavBar() {
 
-  const handleButtonClick = () => {
-    setShowModal(true);
-  };
-    const user = useAuth();
+	const [showModal, setShowModal] = useState(false);
+	const handleModalClose = () => {
+		setShowModal(false);
+	};
+
+	const handleButtonClick = () => {
+		setShowModal(true);
+	};
+	const user = useAuth();
 	//console.log(user)
 	const navigate = useNavigate();
 	const axios = useAxios();
+	const protectedAxios = useAxios();
 	const handleLogout = async () => {
 		try {
 			await axios.get("/api/logout");
@@ -36,64 +44,92 @@ export default function DashboardNavBar(){
 		}
 
 	};
-	
-	
-	return (			
+
+	const handleDocumentCreation: React.FormEventHandler<
+		HTMLFormElement
+	> = async (event) => {
+		event.preventDefault();
+		const documentName = event.target["documentName"].value;
+		console.log(documentName);
+		const data = await protectedAxios.post("/api/document", {
+			documentName,
+		});
+		console.log("Document Created");
+		const documentID: string = data.data;
+		const groupKeys = await cryptoUtils.generateGroupKeys();
+		await cryptoUtils.saveGroupKeysToIDB(groupKeys, documentID);
+		await sendGroupKeyToServer(documentID, protectedAxios);
+		navigate("/document/" + documentID, {
+			replace: true,
+			state: {
+				documentName,
+				newDocumentCreation: true,
+				documentID,
+			},
+		});
+	};
+
+
+	return (
 		<Navbar bg="dark" variant="dark" expand="lg">
-       		<Container>
-         		<Navbar.Brand href="/">
-					<img style={{objectFit:"contain"}}
-     						src="/logo_30_dark.png"
-     						width="auto"
-     						height="auto"      						     						
-    				/>{' '}					
+			<Container>
+				<Navbar.Brand href="/">
+					<img style={{ objectFit: "contain" }}
+						src="/logo_30_dark.png"
+						width="auto"
+						height="auto"
+					/>{' '}
 				</Navbar.Brand>
-                <Navbar.Text>
-                    Dashboard
-                </Navbar.Text>
+				<Navbar.Text>
+					Dashboard
+				</Navbar.Text>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-       			<Navbar.Collapse id="basic-navbar-nav">
-         			<Nav className="ms-auto">           			            			
-         			</Nav>
-                    <Button onClick={handleButtonClick} >                        
-                        New Document
-                    </Button>
-                    <Modal show={showModal} onHide={handleModalClose}>                      
-                      <Modal.Header closeButton>
-                        <Modal.Title>Create New Document</Modal.Title>
-                      </Modal.Header>
-                      <Modal.Body>
-                        {/* Modal body content */}
-                      </Modal.Body>
-                      <Modal.Footer>
-                        <Button variant="secondary" onClick={handleModalClose}>
-                          Close
-                        </Button>
-                        <Button variant="primary" onClick={handleModalClose}>
-                          Create
-                        </Button>
-                      </Modal.Footer>
-                    </Modal>
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ms-auto">
+					</Nav>
+					<Button onClick={handleButtonClick} >
+						New Document
+					</Button>
+					<Modal show={showModal} onHide={handleModalClose}>
+						<Modal.Header closeButton>
+							<Modal.Title>Create New Document</Modal.Title>
+						</Modal.Header>
+						<Modal.Body>
+							<form onSubmit={handleDocumentCreation}>
+								<label>
+									Document Name:
+									<input type="text" name="documentName" />
+								</label>
+								<button type="submit">Create Document</button>
+							</form>
+						</Modal.Body>
+						<Modal.Footer>
+							<Button variant="secondary" onClick={handleModalClose}>
+								Close
+							</Button>
+						</Modal.Footer>
+					</Modal>
 					<NavDropdown title={
 						<Image
-    						src={'https://github.com/nightink.png'}
-    						alt="User profile image"
-    						roundedCircle
-    						style={{ width: '25px' }}
-  						/>
-  					} style={{color:"white",margin: "1rem",}}id="collasible-nav-dropdown">
-              			<NavDropdown.Item href="#action/3.1" className="text-center" style={{fontWeight:"bold"}}>{user.userData?.userName}</NavDropdown.Item>               			
-              			<NavDropdown.Divider />   
-						<NavDropdown.Item className="text-center">           		
-                		<Button variant="primary" onClick={handleLogout}  
-						style={{textDecoration: "none",
-  								}}>
+							src={'https://github.com/nightink.png'}
+							alt="User profile image"
+							roundedCircle
+							style={{ width: '25px' }}
+						/>
+					} style={{ color: "white", margin: "1rem", }} id="collasible-nav-dropdown">
+						<NavDropdown.Item href="#action/3.1" className="text-center" style={{ fontWeight: "bold" }}>{user.userData?.userName}</NavDropdown.Item>
+						<NavDropdown.Divider />
+						<NavDropdown.Item className="text-center">
+							<Button variant="primary" onClick={handleLogout}
+								style={{
+									textDecoration: "none",
+								}}>
 								Logout
-						</Button>  
-						</NavDropdown.Item>            			
-            		</NavDropdown>
+							</Button>
+						</NavDropdown.Item>
+					</NavDropdown>
 				</Navbar.Collapse>
-       		</Container>
-     		</Navbar>
+			</Container>
+		</Navbar>
 	);
 }
